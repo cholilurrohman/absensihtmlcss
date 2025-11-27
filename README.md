028cf3f59d0185ff4d8acd3ed845863ce5f9f143
cholilurrohman <67357408+cholilurrohman@users.noreply.github.com>
2025-11-27 12:39:58 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# absensihtmlcss
-sebuah aplikasi sederhana html tampilan mobile
+# aplikasiSederhana-AbsensiSekolah
+Aplikasi ini dibuat sebagai syarat Tugas Akhir Sekolah
+
+Deskripsi Aplikasi:
+
+Aplikasi ini digunakan untuk mencatat kehadiran siswa siswi SMK Negeri 2 kota Bandung
+- Siswa cukup memasukan Nomor Induk Siswa yang dimiliki 
+- admin dapat melihat rekap kehadiran siswa , mencetak kehadiran siswa
+- admin dapat mengelola data siswa
+
+nama Database : ta
+
+admin account
+id : admin
+pass : admin
---
+++ TampilanKehadiran.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+    <title> SMK Sirotol Mustaqim </title>
+    <link rel="shortcut icon" href="../img/Logo-SMKN2.png" />
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href='../assets/css/bootstrap.css' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+    <div class="container">
+        <div class="panel panel-info" style="margin-top: 100px">
+            <div class="panel-heading">
+                <table border="0" width="100%" style="text-align:center">
+                    <tr>
+                        <td><img src="../img/1.png" height="200" width="200"> </img></td>
+                        <td>
+                            <h1> SMK SMK Sirotol Mustaqim </h1><br>
+                            Jalan in aja dulu Nomor 4,Telepon 022-7234285, <br>
+                            Faksimil 022-4231857, Website www.smksirotolmustaqim.sch.id <br>
+                            email : humas@smksirotolmustaqim.sch.id <br>
+                            Kota Malang - 40114
+                        </td>
+                        <td><img src="../img/jatim.PNG" height="200" width="200"> </img></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="panel-body">
+                <form method="POST" action="ProsesKehadiran.php" class="form-horizontal">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="glyphicon glyphicon-user"> </i>
+                        </span>
+                        <input type="text" name="NIS" placeholder="Masukan Nomor Induk Siswa"
+                            class="form-control input-lg text-center" required>
+                    </div>
+            </div>
+            <div class="panel-Footer">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="../index.php" class="btn btn-success btn-lg btn-block">
+                            <span class="glyphicon glyphicon-home"> </span> Kembali
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <button type="Submit" name="Hadir" value="Masuk" class="btn btn-primary btn-lg btn-block">
+                            <span class="glyphicon glyphicon-info-sign"> </span> Absen
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
